ea65f06a2317f91f5d575bf5e401e8c597271cc7
Diego Ramos <diegorramos84@gmail.com>
2023-10-30 20:56:03 +0000

    fix extra tests till 25

--- 02-json_parser/json_parser/main.py
+++ 02-json_parser/json_parser/main.py
@@ -7,25 +7,7 @@ from typing_extensions import Annotated
 
 app = typer.Typer()
 
-
-# def is_valid_json(content):
-#     if not content or content.isspace():
-#         return False
-
-#     stack = []
-
-#     for char in content:
-#         if char in "{[":
-#             stack.append(char)
-#         elif char in "]}":
-#             if not stack:
-#                 return False
-#             if char == "}" and stack[-1] != "{":
-#                 return False
-#             if char == "]" and stack[-1] != "[":
-#                 return False
-#             stack.pop()
-#     return not stack
+MAX_ALLOWED_DEPTH = 10
 
 
 def is_valid_json(content):
@@ -35,6 +17,11 @@ def is_valid_json(content):
     stack = []  # Stack to keep track of opening and closing brackets
     in_string = False  # Flag to indicate if we are inside a string
     escape = False  # Flag to indicate if the current character is escaped
+    last_char = ""
+    depth = 0
+
+    # check if jason starts with "" instead of {} or []
+    is_payload_string = content.startswith('"') and content.endswith('"')
 
     for char in content:
         if in_string:
@@ -51,6 +38,7 @@ def is_valid_json(content):
                 in_string = True
             elif char in ("{", "[", "("):
                 stack.append(char)
+                depth += 1  # increment nesting depth by 1
             elif char in ("}", "]", ")"):
                 if not stack:
                     return False
@@ -61,8 +49,13 @@ def is_valid_json(content):
                     or (char == ")" and opening_bracket != "(")
                 ):
                     return False
-
-    return not stack and not in_string
+                depth -= 1  # it means we closed a bracket so reduce nesting depth
+            elif not char.isspace() and not stack:
+                return False
+        if depth > MAX_ALLOWED_DEPTH:
+            print(f"Error: nesting level above max allowed: {depth}")
+            return False
+    return not stack and not in_string and not is_payload_string
 
 
 def check_valid_json(file):
@@ -70,6 +63,16 @@ def check_valid_json(file):
         content = file.read()
 
     return is_valid_json(content)
+
+
+def check_content_outbounds(file):
+    with open(file, "r") as file:
+        content = file.read()
+    if content[-1] == '"':
+        print(f"the JSON file {content} ends with a quotation mark")
+        return False
+    else:
+        return True
 
 
 def find_unquoted_keys(file):
@@ -83,6 +86,11 @@ def find_unquoted_keys(file):
             print("The following keys must be quoted:")
             for key in unquoted_keys:
                 print(key)
+                return False
+
+        if ' , "' in content:
+            print("Found missing value")
+            return False
         else:
             return True
 
@@ -113,18 +121,6 @@ def check_for_trailing_commas(file):
             return True
 
 
-# def contains_no_illegal_expression(file):
-#     with open(file, "r") as myFile:
-#         content = myFile.read()
-#         try:
-#             parsed = ast.literal_eval(content)
-#             if parsed:
-#                 return True
-#         except Exception as e:
-#             print(f"Illegal expression found: {e}")
-#             return False
-
-
 def check_for_illegal_chars(filename):
     with open(filename, "r") as file:
         content = file.read()
@@ -153,7 +149,54 @@ def contains_hexadecimal(file):
     return True
 
 
+# check for '\t' chars
+def contains_tab_in_string(string):
+    in_string = False
+    escaped = False
+
+    for char in string:
+        if char == "\\" and not escaped:
+            escaped = True
+        elif char == '"' and not escaped:
+            in_string = not in_string
+        elif char == "t" and not escaped and in_string:
+            return True
+        elif char == "\t" and in_string:
+            return True
+        else:
+            escaped = False
+
+    return False
+
+
+def contains_line_break_in_string(string):
+    in_string = False
+    escaped = False
+
+    for char in string:
+        if char == "\\" and not escaped:
+            escaped = True
+        elif char == '"' and not escaped:
+            in_string = not in_string
+        elif char == "n" and not escaped and in_string:
+            return True
+        else:
+            escaped = False
+
+    return False
+
+
 def parse_value(value, my_dict, key):
+    # if contains_tab_in_string(value):
+    #     print(f"Error mid parsing: the value '{value}' contains a tab character")
+    #     sys.exit(1)
+    # if contains_line_break_in_string(value):
+    #     print(f"Error mid parsing: the value '{value}' contains a line break character")
+    #     sys.exit(1)
+    # nested lists
+    if isinstance(value, list):
+        value = [parse_value(elem, {}, None) for elem in value]
+    # nested objects
     if value.startswith("{") and value.endswith("}"):
         value = ast.literal_eval(value)
         my_dict[key] = value
@@ -164,13 +207,46 @@ def parse_value(value, my_dict, key):
         if value.startswith("[") and value.endswith("]"):
             value = ast.literal_eval(value)
         elif value.isdigit():
-            value = int(value)
+            # check for digits with leading zeroes
+            if value.startswith("0") and len(value) > 1:
+                print(
+                    f"Error mid parsing: the value for key '{key}' cannot have leading zeroes"
+                )
+                sys.exit(1)
+            else:
+                value = int(value)
         elif value == "null":
             value = None
         elif value == "true":
             value = True
         elif value == "false":
             value = False
+
+        # check for math without quotes
+        elif (
+            value[0] != '"'
+            and value[-1] != '"'
+            and any(op in value for op in ["+", "-", "*", "/", "%", "**"])
+        ):
+            print(
+                f"Error mid parsing: the value for key '{key}' contains a mathematical operation without quotes"
+            )
+            sys.exit(1)
+
+        # check for illegal evocations
+        elif "()" in value:
+            print(
+                f"Error mid parsing: the value for key '{key}' contains an illegal invocation"
+            )
+            sys.exit(1)
+
+        # check for hex numbers
+
+        elif value.startswith("0x"):
+            print(
+                f"Error mid parsing: the value for key '{key}' is a hexadecimal number"
+            )
+            sys.exit(1)
         my_dict[key] = value
     return my_dict
 
@@ -196,51 +272,92 @@ def split_json_string(json_str):
     return result
 
 
+# def clean_content(content):
+#     cleaned_lines = []
+
+#     in_string = False
+
+#     for line in content.split("\n"):
+#         if not in_string:
+#             line = line.strip()
+#             in_string = line.startswith('"') and not line.endswith('"')
+#         cleaned_lines.append(line)
+
+#     cleaned_content = "\n".join(cleaned_lines)
+#     return cleaned_content
+
+
 def parser(file):
     with open(file, "r") as myFile:
         content = myFile.read()
 
         cleaned_content = re.sub(r"\n\s*", "", content)
+        print(cleaned_content, "CLEAN")
 
         pairs = split_json_string(cleaned_content)
 
         my_dict = {}
 
         for pair in pairs:
-            key, value = map(str.strip, pair.rstrip(",").split(":", 1))
-            key = key.strip('{"')
+            if pair[0] == "[" and pair[-1] == "]" and ":" in pair:
+                print(f"Error mid parsing: list contains colons instead of commas")
+                sys.exit(1)
 
-            check_for_wrong_boolean_format(file, value)
+            if ":" not in pair and "," in pair:
+                print(
+                    f"Invalid JSON: Found a comma instead of a colon in pair '{pair}'"
+                )
+                sys.exit(1)
 
-            parse_value(value, my_dict, key)
+            if ":" in pair:
+                key, value = map(str.strip, pair.rstrip(",").split(":", 1))
+                key = key.strip('{"')
+
+                # if key-pair had a double colon, one will be carried over
+                if ":" in key or ":" in value[0]:
+                    print(f"Error mid parsing: double colon")
+                    sys.exit(1)
+
+                check_for_wrong_boolean_format(file, value)
+
+                parse_value(value, my_dict, key)
+            elif ":" not in pair and pair[0] == "{" and pair[-1] == "}":
+                print(f"Error mid parsing: missing colon")
+                sys.exit(1)
+            else:
+                parse_value(pair, my_dict, None)
         return my_dict
 
 
 def validate_json(filename):
-    is_valid = check_valid_json(filename)
-    has_no_apostrophes = check_for_apostrophe(filename)
-    has_no_unquoted_keys = find_unquoted_keys(filename)
-    has_no_trailing_commas = check_for_trailing_commas(filename)
-    has_no_illegal_chars = check_for_illegal_chars(filename)
-    has_no_hexadecimal = contains_hexadecimal(filename)
-    print(is_valid, "TEST")
+    try:
+        is_valid = check_valid_json(filename)
+        has_no_apostrophes = check_for_apostrophe(filename)
+        has_no_unquoted_keys = find_unquoted_keys(filename)
+        has_no_trailing_commas = check_for_trailing_commas(filename)
+        has_no_illegal_chars = check_for_illegal_chars(filename)
+        has_no_hexadecimal = contains_hexadecimal(filename)
+        has_no_extra_content = check_content_outbounds(filename)
 
-    return (
-        is_valid
-        and has_no_apostrophes
-        and has_no_unquoted_keys
-        and has_no_trailing_commas
-        and has_no_illegal_chars
-        and has_no_hexadecimal
-    )
+        return (
+            is_valid
+            and has_no_apostrophes
+            and has_no_unquoted_keys
+            and has_no_trailing_commas
+            and has_no_illegal_chars
+            and has_no_hexadecimal
+            and has_no_extra_content
+        )
+    except Exception as e:
+        print(f"Error during JSON validation: {e}")
+        return False
 
 
 @app.command()
 def main(filename: Annotated[str, typer.Argument()]):
     if filename and validate_json(filename):
-        print("Valid JSON")
+        print("Initials checks: PASS")
         return parser(filename)
-
     else:
         print("Invalid JSON")
         sys.exit(1)
--- 02-json_parser/tests/test_extra.py
+++ 02-json_parser/tests/test_extra.py
@@ -2,77 +2,26 @@
 # import os
 
 # import pytest
-
-# from json_parser.main import parser
+# from json_parser.main import main
 
 # # get the current path were the test is being called from
 # current_dir = os.path.dirname(__file__)
 
 # # construct the path to the .json files
-# # valid_json_path = os.path.join(current_dir, "step4", "valid.json")
-# # valid2_json_path = os.path.join(current_dir, "step4", "valid2.json")
-# # valid3_json_path = os.path.join(current_dir, "step4", "valid3.json")
-# # valid4_json_path = os.path.join(current_dir, "step4", "valid4.json")
-
-# # invalid_json_path = os.path.join(current_dir, "step4", "invalid.json")
+# folder_path = os.path.join(current_dir, "extra")
 
 
-# @pytest.fixture
-# def json_load_data(request):
-#     json_file = os.path.join("extra", request.param)
-#     with open(json_file) as f:
-#         data = json.load(f)
-#     return data
+# def get_fail_json_files(dir):
+#     fail_json_files = []
+#     for filename in os.listdir(dir):
+#         if filename.startswith("fail") and filename.endswith(".json"):
+#             fail_json_files.append(os.path.join(dir, filename))
+#     return fail_json_files
 
 
-# def json_parser_data(request):
-#     json_file = os.path.join("extra", request.param)
-#     result = parser(json_file)
-#     return result
-
-
-# def test_empty_object_returns_dict(json_load_data, json_parser_data):
-#     result = json_parser_data
-
-#     data = json_load_data
-
-#     assert result == data
-
-
-# # def test_non_empty_object_returns_dict():
-# #     result = parser(valid2_json_path)
-
-# #     f = open(valid2_json_path)
-
-# #     data = json.load(f)
-
-# #     assert result == data
-
-
-# # # comma inside list
-# # def test_non_empty_object_returns_list_nested():
-# #     result = parser(valid3_json_path)
-
-# #     f = open(valid3_json_path)
-
-# #     data = json.load(f)
-
-# #     assert result == data
-
-
-# # # comma inside dict
-# # def test_non_empty_object_returns_dict_nested():
-# #     result = parser(valid4_json_path)
-
-# #     f = open(valid4_json_path)
-
-# #     data = json.load(f)
-
-# #     assert result == data
-
-
-# def test_non_empty_object_returns_dict_wrong_format():
+# @pytest.mark.parametrize("invalid_json_path", get_fail_json_files(folder_path))
+# def test_non_empty_object_returns_dict_wrong_format(invalid_json_path):
 #     with pytest.raises(SystemExit) as result:
-#         parser(invalid_json_path)
+#         main(invalid_json_path)
 #     assert result.type == SystemExit
 #     assert result.value.code == 1
